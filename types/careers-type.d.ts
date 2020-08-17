99e3ebb3ea43ff285eae095d90bbc4089344e4cc
naoto-koyama <endless324@gmail.com>
2020-08-17 20:59:20 +0900

    mod type from graphql-types

--- types/careers-type.d.ts
+++ types/careers-type.d.ts
@@ -1,14 +1,6 @@
-import { Maybe, Scalars } from './graphql-types'
+import { MicrocmsCareers } from './graphql-types'
 
-export type CareerType = {
-  id: Scalars['ID']
-  createdAt?: Maybe<Scalars['Date']>
-  updatedAt?: Maybe<Scalars['Date']>
-  publishedAt?: Maybe<Scalars['Date']>
-  careerName?: Maybe<Scalars['String']>
-  role?: Maybe<Scalars['String']>
-  fromDate?: Maybe<Scalars['Date']>
-  toDate?: Maybe<Scalars['Date']>
-  description?: Maybe<Scalars['String']>
-  careersId?: Maybe<Scalars['String']>
-}
+export type CareerType = Pick<
+  MicrocmsCareers,
+  'id' | 'fromDate' | 'toDate' | 'careerName' | 'role' | 'description'
+>
--- types/user-info-type.d.ts
+++ types/user-info-type.d.ts
@@ -1,15 +1,16 @@
-import { Maybe, MicrocmsUserinfoAvatar, Scalars } from './graphql-types'
+import { MicrocmsUserinfo } from './graphql-types'
 
-export type UserinfoType = {
-  id: Scalars['ID']
-  createdAt?: Maybe<Scalars['Date']>
-  updatedAt?: Maybe<Scalars['Date']>
-  publishedAt?: Maybe<Scalars['Date']>
-  name?: Maybe<Scalars['String']>
-  role?: Maybe<Scalars['String']>
-  birthday?: Maybe<Scalars['Date']>
-  address?: Maybe<Scalars['String']>
-  email?: Maybe<Scalars['String']>
-  greetingText?: Maybe<Scalars['String']>
-  avatar?: Maybe<MicrocmsUserinfoAvatar>
-}
+export type UserinfoType = Pick<
+  MicrocmsUserinfo,
+  | 'id'
+  | 'createdAt'
+  | 'updatedAt'
+  | 'publishedAt'
+  | 'name'
+  | 'role'
+  | 'birthday'
+  | 'address'
+  | 'email'
+  | 'greetingText'
+  | 'avatar'
+>
